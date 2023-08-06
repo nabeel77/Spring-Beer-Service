a9816938f555de5379891e89ce2dccd61390888d
Nabeel77 <nabeel.beeni30@gmail.com>
2023-08-06 13:43:12 +0300

    Added test for list beer endpoint

--- src/test/java/com/spring6mvcrest/spring6mvcrest/controller/BeerControllerTest.java
+++ src/test/java/com/spring6mvcrest/spring6mvcrest/controller/BeerControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.print.attribute.standard.Media;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
@@ -30,6 +31,18 @@ class BeerControllerTest {
     BeerService beerService;
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testListBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
     @Test
     void getBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.listBeers().get(0);
