c76ace4a3890ac28fa7c850600f5b537d1866384
Nabeel77 <nabeel.beeni30@gmail.com>
2023-08-19 11:55:56 +0300

    Added value property in responseStatus annotation in NotFoundException class

--- src/main/java/com/spring6mvcrest/spring6mvcrest/controller/NotFoundException.java
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/controller/NotFoundException.java
@@ -3,7 +3,7 @@ package com.spring6mvcrest.spring6mvcrest.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
