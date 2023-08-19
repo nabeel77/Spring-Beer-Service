0304ee573cf19a1848b9766a29a2cb8b715bfcf8
Nabeel77 <nabeel.beeni30@gmail.com>
2023-08-19 11:55:16 +0300

    Added responseStatus annotation on NotFoundException class

--- src/main/java/com/spring6mvcrest/spring6mvcrest/controller/NotFoundException.java
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.spring6mvcrest.spring6mvcrest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
