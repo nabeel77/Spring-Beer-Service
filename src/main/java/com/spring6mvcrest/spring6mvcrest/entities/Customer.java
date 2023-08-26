a31f8d0daa0d99f1e366a02e909e77e2b4985228
Nabeel77 <nabeel.beeni30@gmail.com>
2023-08-26 15:08:52 +0300

    Added Hiberante mapping entities

--- src/main/java/com/spring6mvcrest/spring6mvcrest/entities/Customer.java
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/entities/Customer.java
@@ -1,9 +1,8 @@
 package com.spring6mvcrest.spring6mvcrest.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,7 +16,11 @@ import java.util.UUID;
 public class Customer {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
     private String customerName;
 
     @Version
