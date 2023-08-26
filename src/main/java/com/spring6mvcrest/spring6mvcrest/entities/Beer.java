2eeb2d2be7190160de3d7b519ef5e930031e9795
Nabeel77 <nabeel.beeni30@gmail.com>
2023-08-26 15:12:12 +0300

    Added Hiberante mapping entities for Beer Object

--- src/main/java/com/spring6mvcrest/spring6mvcrest/entities/Beer.java
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/entities/Beer.java
@@ -1,10 +1,9 @@
 package com.spring6mvcrest.spring6mvcrest.entities;
 
 import com.spring6mvcrest.spring6mvcrest.model.BeerStyle;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -19,6 +18,9 @@ import java.util.UUID;
 
 public class Beer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
     @Version
