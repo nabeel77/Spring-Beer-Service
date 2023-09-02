66067e13b18bec2d3703e3c3979d8a32bdba3728
Nabeel77 <nabeel.beeni30@gmail.com>
2023-09-02 14:14:44 +0300

    Added CustomerRepositoryTest and test method to save the customer.

---
+++ src/test/java/com/spring6mvcrest/spring6mvcrest/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.spring6mvcrest.spring6mvcrest.repositories;
+
+import com.spring6mvcrest.spring6mvcrest.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void saveCustomer() {
+        Customer savedCustomer = customerRepository.save(Customer.builder().name("Ali").build());
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+}
