8c5b3dfd715a9e5ced0012abdbf1377752420e9d
Nabeel77 <nabeel.beeni30@gmail.com>
2023-09-17 13:17:56 +0300

    Added CustomerServiceImplJPA class and marked it as primary impl for Customer service

---
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/service/CustomerServiceImplJPA.java
@@ -0,0 +1,51 @@
+package com.spring6mvcrest.spring6mvcrest.service;
+
+import com.spring6mvcrest.spring6mvcrest.mappers.CustomerMapper;
+import com.spring6mvcrest.spring6mvcrest.model.CustomerDTO;
+import com.spring6mvcrest.spring6mvcrest.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceImplJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomer() {
+        return null;
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updatedCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void updatedCustomerPatchById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
