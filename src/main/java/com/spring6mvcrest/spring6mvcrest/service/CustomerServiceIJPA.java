2e2920be586687f4a849672344560314ff8a00ef
Nabeel77 <nabeel.beeni30@gmail.com>
2023-09-17 13:39:09 +0300

    Implemented save, update, get methods for CustomerServiceIJPA

--- src/main/java/com/spring6mvcrest/spring6mvcrest/service/CustomerServiceIJPA.java
+++ src/main/java/com/spring6mvcrest/spring6mvcrest/service/CustomerServiceIJPA.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -21,22 +23,30 @@ public class CustomerServiceIJPA implements CustomerService {
 
     @Override
     public List<CustomerDTO> listCustomer() {
-        return null;
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id).orElse(null)));
     }
 
     @Override
     public CustomerDTO saveNewCustomer(CustomerDTO customer) {
-        return null;
+        return customerMapper.customerToCustomerDto(customerRepository.save(customerMapper.customerDtoToCustomer(customer)));
     }
 
     @Override
     public void updatedCustomerById(UUID customerId, CustomerDTO customer) {
-
+        customerRepository.findById(customerId).ifPresent(foundCustomer -> {
+            foundCustomer.setCustomerName(customer.getCustomerName());
+            foundCustomer.setVersion(Integer.parseInt(customer.getVersion()));
+            foundCustomer.setUpdatedDate(LocalDateTime.now());
+            customerRepository.save(foundCustomer);
+        });
     }
 
     @Override
